2a8f0ffc34090f25ddd4fa378ab8fadc02711b74
AlfonzM <m.alfonz@gmail.com>
2022-09-17 14:09:08 +0800

    [vim] Update indent blankline settings

--- nvim/config/indent-blankline.lua
+++ nvim/config/indent-blankline.lua
@@ -1,8 +1,11 @@
 require("indent_blankline").setup {
-  show_current_context = true,
+  show_current_context = false,
   indent_blankline_use_treesitter = true,
-  indent_blankline_show_current_context = true,
+  indent_blankline_show_current_context = false,
 
   -- Align with bufferline and gitsigns
   indent_blankline_context_char = '▎',
 }
+
+vim.cmd[[highlight IndentBlanklineChar guifg=#333333]]
+ 

f64df5ad10a4675b50151be87353e7953ac9f1d6
AlfonzM <m.alfonz@gmail.com>
2022-10-24 23:39:12 +0800

    [vim] Update lualine colors

--- nvim/.config/nvim/lua/alfonzm/plugins/lualine.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/lualine.lua
@@ -1,11 +1,80 @@
 local lsp_status = require('lsp-status')
 
+-- Gruvbox
+local colors = {
+  black        = '#282828',
+  white        = '#ebdbb2',
+  red          = '#fb4934',
+  green        = '#b8bb26',
+  blue         = '#83a598',
+  yellow       = '#fe8019',
+  gray         = '#a89984',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+}
+
+local default_color = {bg = colors.black, fg = colors.inactivegray}
+
+local gruvbox = {
+  normal = {
+    a = {bg = colors.gray, fg = colors.black, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  },
+  insert = {
+    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  },
+  visual = {
+    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  },
+  replace = {
+    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  },
+  command = {
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  },
+  inactive = {
+    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+    b = default_color,
+    c = default_color,
+    x = default_color,
+    y = default_color,
+    z = default_color
+  }
+}
+
 -- Register the progress handler
 lsp_status.register_progress()
 
+-- local default_color = { bg = colors.black, fg = colors.inactivegray }
+
 require('lualine').setup({
     options = {
-        theme = 'gruvbox',
+        theme = gruvbox,
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = { 'vim-plug', 'NvimTree' },
@@ -17,7 +86,7 @@ require('lualine').setup({
         },
         lualine_c = {},
         lualine_x = {},
-        lualine_y = {},
+        lualine_y = { 'branch' },
         lualine_z = { "require'lsp-status'.status()" },
     },
     inactive_sections = {

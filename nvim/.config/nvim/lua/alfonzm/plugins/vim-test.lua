e43c425b6d3babefd2e6b4865f766421f0b25cd3
AlfonzM <m.alfonz@gmail.com>
2022-10-24 21:25:59 +0800

    [vim] Fix vim test position

--- nvim/.config/nvim/lua/alfonzm/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/vim-test.lua
@@ -4,5 +4,6 @@ vim.keymap.set('n', '<Leader>ts', ':TestSuite<CR>')
 vim.keymap.set('n', '<Leader>tl', ':TestLast --verbose<CR>')
 vim.keymap.set('n', '<Leader>tv', ':TestVisit --verbose<CR>')
 
+vim.g['test#neovim#term_position'] = 'vert'
 vim.g['test#php#phpunit#executable'] = 'vendor/bin/phpunit'
 vim.g['test#strategy'] = 'neovim'

962641667dd3621ff64eb55f929f3e72a1301d38
AlfonzM <m.alfonz@gmail.com>
2022-11-05 12:39:07 +0800

    [vim] Add fzf binding for Buffers

--- nvim/.config/nvim/lua/alfonzm/plugins/fzf.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/fzf.lua
@@ -3,3 +3,4 @@ vim.cmd[[let $FZF_DEFAULT_OPTS="--margin=1,4 --preview \'bat --color=always --st
 vim.keymap.set('n', '<C-p>', ':Files<CR>')
 vim.keymap.set('n', '<C-f>', ':Ag<CR>')
 vim.keymap.set('n', '<Leader>h', ':History<CR>')
+vim.keymap.set('n', '<Leader>b', ':Buffers<CR>')

7f56674497d93fa7c30a9e349700e73d2a4892c0
AlfonzM <m.alfonz@gmail.com>
2022-12-18 10:53:03 +0800

    [vim] Temporary fix for nvim-tree bg color issue

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -7,24 +7,33 @@ vim.fn.sign_define("DiagnosticSignWarn", { text = '•', texthl = 'yellow', line
 vim.fn.sign_define("DiagnosticSignHint", { text = '•', texthl = 'yellow', linehl = '', numhl = '' })
 vim.fn.sign_define("DiagnosticSignInfo", { text = '•', texthl = 'yellow', linehl = '', numhl = '' })
 
+-- Temporary fix for Nvim-Tree bg color
+vim.api.nvim_exec(
+[[
+augroup MyColors
+autocmd!
+autocmd BufNewFile,BufRead * hi NvimTreeNormal guifg=NONE guibg=NONE
+augroup END
+]], false)
+
 vim.api.nvim_exec(
     [[
-    augroup-theme-overrides
-        autocmd!
-        autocmd ColorScheme gruvbox-material highlight Folded ctermfg=LightGray guifg=#918d88
-        autocmd ColorScheme gruvbox-material highlight Folded ctermbg=235 guibg=NONE
-        autocmd ColorScheme gruvbox-material highlight Folded cterm=italic gui=italic
-        autocmd ColorScheme gruvbox-material highlight SignColumn ctermbg=NONE guibg=NONE
-        autocmd ColorScheme gruvbox-material highlight DiagnosticSign ctermbg=235 guibg=NONE
-
-        " NvimTree
-        autocmd ColorScheme gruvbox-material highlight NvimTreeNormal guifg=Normal guibg=NONE
-        autocmd ColorScheme gruvbox-material highlight NvimTreeEndOfBuffer guifg=#7c6f64 guibg=NONE
-        " autocmd ColorScheme gruvbox-material highlight NvimTreeFolderName guifg=#7c6f64
-        " autocmd ColorScheme gruvbox-material highlight NvimTreeOpenedFolderName guifg=#ebdbb2
-        " autocmd ColorScheme gruvbox-material highlight NvimTreeEmptyFolderName guifg=#ebdbb2
-        autocmd ColorScheme gruvbox-material highlight NvimTreeOpenedFile guifg=#ebdbb2
-    augroup END
+    " augroup MyColors
+    "     autocmd!
+    "     autocmd ColorScheme * highlight Folded ctermfg=LightGray guifg=#918d88
+    "     autocmd ColorScheme * highlight Folded ctermbg=235 guibg=NONE
+    "     autocmd ColorScheme * highlight Folded cterm=italic gui=italic
+    "     autocmd ColorScheme * highlight SignColumn ctermbg=NONE guibg=NONE
+    "     autocmd ColorScheme * highlight DiagnosticSign ctermbg=235 guibg=NONE
+    "
+    "     " NvimTree
+    "     autocmd ColorScheme * highlight NvimTreeNormal guifg=NONE guibg=NONE
+    "     autocmd ColorScheme * highlight NvimTreeEndOfBuffer guifg=#7c6f64 guibg=NONE
+    "     " autocmd ColorScheme * highlight NvimTreeFolderName guifg=#7c6f64
+    "     " autocmd ColorScheme * highlight NvimTreeOpenedFolderName guifg=#ebdbb2
+    "     " autocmd ColorScheme * highlight NvimTreeEmptyFolderName guifg=#ebdbb2
+    "     autocmd ColorScheme * highlight NvimTreeOpenedFile guifg=#ebdbb2
+    " augroup END
     let g:gruvbox_material_diagnostic_text_highlight = 1
     let g:gruvbox_material_diagnostic_line_highlight = 1
     let g:gruvbox_material_diagnostic_virtual_text   = 1
@@ -45,8 +54,8 @@ vim.cmd [[highlight IncSearch guifg=#282828 guibg=#fabd2f]]
 vim.cmd [[highlight StatusLine guibg=#282828]]
 vim.cmd [[highlight StatusLineNC guibg=#282828]]
 
--- Change NvimTree folder colors, not yet working (TODO)
-vim.cmd [[highlight NvimTreeNormal guifg=Normal guibg=NONE]]
-vim.cmd [[highlight NvimTreeEndOfBuffer guifg=#7c6f64 guibg=NONE]]
+-- Change NvimTree folder colors, not yet working, needs to re-source (TODO)
+-- vim.cmd [[highlight NvimTreeNormal guifg=NONE guibg=NONE]]
+-- vim.cmd [[highlight NvimTreeEndOfBuffer guifg=#7c6f64 guibg=NONE]]
 -- vim.cmd [[highlight NvimTreeOpenedFolderName guifg=#7c6f64]]
 -- vim.cmd [[highlight NvimTreeEmptyFolderName guifg=#7c6f64]]

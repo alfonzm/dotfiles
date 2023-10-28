a303dc2969a05ba6102ade9734d231f3a03489ef
AlfonzM <m.alfonz@gmail.com>
2023-10-28 16:34:49 +0800

    [nvim] Fix duplicate keybind

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -2,4 +2,4 @@
 vim.keymap.set('n', '<Leader>l', ':!/Applications/love.app/Contents/MacOS/love .<CR><CR>', { silent = true })
 
 -- Source nvim config
-vim.keymap.set('n', '<Leader>s', ':source % | source ~/.config/nvim/init.lua<CR>')
+vim.keymap.set('n', '<Leader>S', ':source % | source ~/.config/nvim/init.lua<CR>')

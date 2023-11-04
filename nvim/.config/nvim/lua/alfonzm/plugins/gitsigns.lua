8742e202ce991ba33f95a860010ff78563ed6831
AlfonzM <m.alfonz@gmail.com>
2023-11-05 00:37:43 +0800

    [nvim] Enable gitsigns keymaps

--- nvim/.config/nvim/lua/alfonzm/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/gitsigns.lua
@@ -22,19 +22,19 @@ require('gitsigns').setup{
     end, {expr=true})
 
     -- Actions
-    -- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    -- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    -- map('n', '<leader>hS', gs.stage_buffer)
-    -- map('n', '<leader>hu', gs.undo_stage_hunk)
-    -- map('n', '<leader>hR', gs.reset_buffer)
-    -- map('n', '<leader>hp', gs.preview_hunk)
-    -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage all hunks in current buffer' })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo the last stage_hunk'})
+    map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset entire buffer' })
+    map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview hunk' }) -- TODO: Improve preview UI
+    map('n', '<leader>hb', function() gs.blame_line { full = false } end, { desc = 'Toggle blame line for current line' })
+    -- map('n', '<leader>tb', gs.toggle_current_line_blame )
     -- map('n', '<leader>hd', gs.diffthis)
     -- map('n', '<leader>hD', function() gs.diffthis('~') end)
     -- map('n', '<leader>td', gs.toggle_deleted)
-    --
-    -- -- Text object
-    -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+
+    -- Text object
+    vim.keymap.set({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }

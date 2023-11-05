35fc4264f67622135254a07acfa526efb02b7079
AlfonzM <m.alfonz@gmail.com>
2023-11-05 14:10:26 +0800

    [nvim] Codegpt keymaps

--- nvim/.config/nvim/lua/alfonzm/plugins/codegpt.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/codegpt.lua
@@ -1,4 +1,5 @@
 vim.g.codegpt_openai_api_key = 'sk-'
 
 -- keymap <Leader>c will run :Chat command for the current highlighted text in visual mode for the current range
-vim.keymap.set('v', '<Leader>c', ":Chat ")
+vim.keymap.set('v', '<Leader>e', ":Chat explain<CR>", { desc = 'Let GPT explain code' })
+vim.keymap.set('v', '<Leader>c', ":Chat ", { desc = 'Instruct GPT to edit code' })

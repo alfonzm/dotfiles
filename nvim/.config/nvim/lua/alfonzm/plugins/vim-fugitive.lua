72bb91b97420697c43139c0e9d1be59ace689072
AlfonzM <m.alfonz@gmail.com>
2023-11-15 20:42:29 +0800

    [nvim] Fix Browse issue

--- nvim/.config/nvim/lua/alfonzm/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/vim-fugitive.lua
@@ -1,1 +1,5 @@
+-- required for Browse to work
+-- https://vi.stackexchange.com/questions/38447/vim-fugitive-netrw-not-found-define-your-own-browse-to-use-gbrowse
+vim.cmd [[command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1)]]
+
 vim.keymap.set('n', '<Leader>G', ':GBrowse<CR>')

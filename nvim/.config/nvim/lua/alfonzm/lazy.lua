3e0d216b9eabb98b9cb29981967b4bfc420c7435
AlfonzM <m.alfonz@gmail.com>
2023-11-16 12:07:35 +0800

    [nvim] Use persistence instead of auto-session

--- nvim/.config/nvim/lua/alfonzm/lazy.lua
+++ nvim/.config/nvim/lua/alfonzm/lazy.lua
@@ -144,6 +144,19 @@ local plugins = {
         end
     },
 
+    -- Persistence
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = { options = vim.opt.sessionoptions:get() },
+        -- stylua: ignore
+        keys = {
+            { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+        },
+    },
+
     -- -- Session
     -- {
     --     'rmagatti/auto-session',

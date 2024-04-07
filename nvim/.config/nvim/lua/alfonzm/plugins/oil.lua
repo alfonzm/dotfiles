a77baf4390d9a45b25663d6520fe5f8390c17ebd
AlfonzM <m.alfonz@gmail.com>
2024-04-07 21:25:39 +0800

    [nvim] Update oil config

--- nvim/.config/nvim/lua/alfonzm/plugins/oil.lua
+++ nvim/.config/nvim/lua/alfonzm/plugins/oil.lua
@@ -5,6 +5,7 @@ require("oil").setup({
     view_options = {
         show_hidden = true,
     },
+    skip_confirm_for_simple_edits = true,
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })

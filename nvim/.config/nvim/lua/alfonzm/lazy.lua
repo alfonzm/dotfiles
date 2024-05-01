f2cf1dfcb3183f8871a044e25752056e4e333cf0
AlfonzM <m.alfonz@gmail.com>
2024-05-01 20:20:05 +0800

    [nvim] Markdown config

--- nvim/.config/nvim/lua/alfonzm/lazy.lua
+++ nvim/.config/nvim/lua/alfonzm/lazy.lua
@@ -332,11 +332,12 @@ local plugins = {
     -- },
 
     -- Markdown
+    -- Tabular is required for vim-markdown
     { 'godlygeek/tabular' },
     {
         'preservim/vim-markdown',
         config = function()
-            vim.g.vim_markdown_folding_disabled = 1
+            vim.g.vim_markdown_folding_disabled = 0
             vim.g.vim_markdown_auto_insert_bullets = 1
             vim.g.vim_markdown_new_list_item_indent = 0
         end
